f42e1a66426de6ea797e6f84aaa97d2eab5186df
Umesh Kribanandan <umesh.kribanandan@gmail.com>
2020-05-08 17:10:17 +0530

    Lazy Loading via Suspense

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,13 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 import "./Blog.css";
 import Posts from "./Posts/Posts";
-import FullPost from "./FullPost/FullPost";
-
 import asyncComponent from "../../hoc/AsyncComponent";
 
 // import NewPost from "./NewPost/NewPost";
 
+const FullPost = React.lazy(() => import("./FullPost/FullPost"));
 const AsyncNewPost = asyncComponent(() => {
   return import("./NewPost/NewPost");
 });
@@ -43,7 +42,17 @@ class Blog extends Component {
         <Switch>
           <Route path="/" exact component={Posts} />
           <Route path="/new-post" exact component={AsyncNewPost} />
-          <Route path="/post/:id" exact component={FullPost} />
+          <Route
+            path="/post/:id"
+            exact
+            render={(props) => {
+              return (
+                <Suspense fallback={<div>Fallback Loading ......</div>}>
+                  <FullPost {...props} />
+                </Suspense>
+              );
+            }}
+          />
           <Redirect from="/posts" to="/" />
           <Route component={() => <h1>Not Found</h1>} />
         </Switch>
