bbffe3c4682b0314541a22634f9c1b7ad27a9d5d
Umesh Kribanandan <umesh.kribanandan@gmail.com>
2020-05-08 17:16:03 +0530

    BrowserRouter basename /  is default if exmaple.com/my-app and then has the react app , change the basename in BrowserRouter

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <div className="App">
           <Blog />
           <hr />
